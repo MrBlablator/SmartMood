f9f20ffbd336079cce55ce8dc6c06e1614a46ad1
Rémi Canillas <remi.canillas@gmail.com>
2015-05-15 12:44:39 +0800

    Renamed classes and finalized implementation with Face++ online API. Still problem with face recognition (API ?)

--- src/MyTest/KinectVideoManager.java
+++ src/MyTest/KinectVideoManager.java
@@ -6,6 +6,8 @@ package MyTest;
 
 
 import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -15,7 +17,8 @@ import javax.imageio.ImageIO;
 
 import edu.ufl.digitalworlds.j4k.J4KSDK;
 
-public class MyKinect extends J4KSDK {
+
+public class KinectVideoManager extends J4KSDK {
 
 	int windowWidth;
 	int windowHeight;
@@ -23,8 +26,11 @@ public class MyKinect extends J4KSDK {
 	FileOutputStream f;
 	SmileDetector smiley = new SmileDetector();
 	int smile_counter =0;
+	String serverUrl;
 	
-	
+	public void setServerUrl(String url){
+		serverUrl = url;
+	}
 	
 	public int getWindowWidth() {
 		return windowWidth;
@@ -39,11 +45,21 @@ public class MyKinect extends J4KSDK {
 	}
 	
 
+	public static BufferedImage resize(BufferedImage img, int newW, int newH) {  
+	    BufferedImage dimg = new BufferedImage(newW, newH, img.getType());  
+	    Graphics2D g = dimg.createGraphics();  
+	    g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+	    RenderingHints.VALUE_INTERPOLATION_BILINEAR);  
+	    g.drawImage(img, 0, 0, newW, newH, null);  
+	    g.dispose();  
+	    return dimg;  
+	}  
+	
 	
 	public void writeVideoFramePNG(int video_width, int video_height ,byte array[])
 	{
 		try {
-			f = new FileOutputStream("captured_img.png");
+			f = new FileOutputStream("D:\\xampp\\htdocs\\img\\captured_img.png");
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -57,38 +73,48 @@ public class MyKinect extends J4KSDK {
 			for(int y=0;y<video_height;y++)
 			for(int x=0;x<video_width;x++)
 			{
-				img.setRGB(x,y,(new Color(array[idx+0]&0xFF,array[idx+1]&0xFF,array[idx+2]&0xFF)).getRGB());
+				img.setRGB(x,y,(new Color(array[idx+2]&0xFF,array[idx+1]&0xFF,array[idx+0]&0xFF)).getRGB());
 				idx+=4;
 			}
 			
-			ImageIO.write(img,"PNG", f);
+			BufferedImage scaledImg = resize(img,1920/2,1080/2);
+			ImageIO.write(scaledImg,"PNG", f);
 		} catch (IOException e) {
         	e.printStackTrace();
         }
         System.out.println("Image saved");
         // Upload image here
         
-        smiley.detectSmile("http://i.imgur.com/dtG2Yu5.jpg");
-        if (smiley.getSmile_value()>75)
-        {
-        	smile_counter = smile_counter +1;
-        	System.out.println("Smile detected");
-        	System.out.println(smile_counter+" smile(s) detected");
-        }
-        else
-        {
-        	System.out.println("No smile detected");
-        }
-		}
+	}
 	
 	@Override
 	public void onColorFrameEvent(byte[] color_frame) {
 		
 		windowWidth = getColorWidth();
 		windowHeight = getColorHeight();
+		
+		//windowWidth = 800;
+		//windowHeight = 600;
+		
 		videoBuffer = color_frame;
 		
+	
 		writeVideoFramePNG(windowWidth,windowHeight,color_frame);
+		
+		smiley.detectSmile(serverUrl + "//img//captured_img.png");
+        if (smiley.getSmile_value()>50)
+        {
+        	smile_counter = smile_counter +1;
+        	System.out.println("Smile");
+        	System.out.println(smile_counter+" smile(s) detected");
+        	smile_counter = 0;
+        }
+        else
+        {
+        	System.out.println("No smile");
+        	smile_counter = 0;
+        }
+		
 		
 		
 	}
@@ -106,29 +132,14 @@ public class MyKinect extends J4KSDK {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	public static void main(String[] args)
-	{
-		
-		if(System.getProperty("os.arch").toLowerCase().indexOf("64")<0)
-		{
-			System.out.println("WARNING: You are running a 32bit version of Java.");
-			System.out.println("This may reduce significantly the performance of this application.");
-			System.out.println("It is strongly adviced to exit this program and install a 64bit version of Java.\n");
-		}
-		
-		System.out.println("This program will run for about 20 seconds.");
-		MyKinect kinect=new MyKinect();
-		kinect.start(J4KSDK.COLOR|J4KSDK.DEPTH|J4KSDK.SKELETON);
-		
-		
-		//Sleep for 20 seconds.
-		try {Thread.sleep(20000);} catch (InterruptedException e) {}
-		
-		
-		kinect.stop();		
-		//System.out.println("FPS: "+kinect.counter*1000.0/(new Date().getTime()-kinect.time));
+
+	public void setColorSize(int i, int j) {
+		// TODO Auto-generated method stub
+		windowWidth=i;
+		windowHeight=j;
 	}
+	
+	
 
 }
 
--- src/MyTest/SmileDetector.java
+++ src/MyTest/SmileDetector.java
@@ -30,19 +30,28 @@ public class SmileDetector {
 		//detection/detect
 		try {
 			 result = httpRequests.detectionDetect(new PostParameters().setUrl(image_path));
-			 //System.out.println(result.get("face").get(0).get("attribute"));
+			 System.out.println(result);
 		} catch (FaceppParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return false;
 		}
 		try {
-			String smile =result.get("face").get(0).get("attribute").toString();
-			smile_value = Double.parseDouble(smile.substring(smile.indexOf("value") +7, smile.indexOf("value") +11).trim());
-			//System.out.println(smile_value);
-			System.out.println("Smile detection succeed");
-			System.out.println("Smile confidence: "+smile_value);
+			if(result.get("face").getCount()!=0)
+			{
+				System.out.println(result.get("face").getCount());
+				String smile =result.get("face").get(0).get("attribute").toString();
+				smile_value = Double.parseDouble(smile.substring(smile.indexOf("value") +7, smile.indexOf("value") +11).trim());
+			
+				System.out.println("Smile detection succeed");
+				System.out.println("Smile confidence: "+smile_value);
 			return true;
+			}
+			else
+			{
+				System.out.println("No face detected");
+				return false;
+			}
 		
 		} catch (FaceppParseException e) {
 			// TODO Auto-generated catch block
