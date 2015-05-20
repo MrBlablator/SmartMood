8466f35766df25cb0f7bae4f68a9a7cb4d1f16fb
Rémi Canillas <remi.canillas@gmail.com>
2015-05-20 19:16:39 +0800

    Added GUI interface and report. Kinect working again. To do : Write comments + optimisation (parallelize ?)

--- src/MyTest/KinectVideoManager.java
+++ src/MyTest/KinectVideoManager.java
@@ -9,14 +9,18 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
 import edu.ufl.digitalworlds.j4k.J4KSDK;
 
+import javax.swing.JTextArea;
 
 public class KinectVideoManager extends J4KSDK {
 
@@ -24,11 +28,41 @@ public class KinectVideoManager extends J4KSDK {
 	int windowHeight;
 	byte[] videoBuffer;
 	FileOutputStream exported_img;
-	FileOutputStream saved_img;
+	File report;
 	SmileDetector smiley = new SmileDetector();
 	int smile_counter =0;
 	String serverUrl;
 	int img_id = 0;
+	JTextArea infoDisplay = null;
+	BufferedWriter writer = null;
+	
+	public KinectVideoManager(){
+		report = new File("report.txt");
+		System.out.print("report created");
+		 try {
+			writer = new BufferedWriter(new FileWriter(report,true));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 try {
+			writer.write("Report of smile detection\r\n\r\n");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			try {
+				writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	public void setInfoDisplay(JTextArea textBox){
+		infoDisplay = textBox;
+	}
 	
 	public void setServerUrl(String url){
 		serverUrl = url;
@@ -61,7 +95,7 @@ public class KinectVideoManager extends J4KSDK {
 	public void writeVideoFramePNG(int video_width, int video_height ,byte array[], int frame_id)
 	{
 		try {
-			exported_img = new FileOutputStream("D:\\xampp\\htdocs\\img\\captured_img.png"+frame_id);
+			exported_img = new FileOutputStream("D:\\xampp\\htdocs\\img\\captured_img"+frame_id+".png");
 			//org_img = new FileOutputStream(".\\video_stream\\img_"+frame_id);
 			
 		} catch (FileNotFoundException e1) {
@@ -87,6 +121,8 @@ public class KinectVideoManager extends J4KSDK {
 		} catch (IOException e) {
         	e.printStackTrace();
         }
+		
+		infoDisplay.append("Image saved\n");
         System.out.println("Image saved");
         // Upload image here
         
@@ -95,6 +131,9 @@ public class KinectVideoManager extends J4KSDK {
 	@Override
 	public void onColorFrameEvent(byte[] color_frame) {
 		
+		try {
+		writer = new BufferedWriter(new FileWriter(report,true));
+		
 		windowWidth = getColorWidth();
 		windowHeight = getColorHeight();
 		
@@ -102,30 +141,81 @@ public class KinectVideoManager extends J4KSDK {
 		//windowHeight = 600;
 		
 		videoBuffer = color_frame;
+		infoDisplay.append("Starting smile detection...\n");
 		
-	
 		writeVideoFramePNG(windowWidth,windowHeight,color_frame,img_id);
 		String path = serverUrl + "//img//captured_img"+img_id+".png";
+		
+		try {
+			writer.write("captured_img"+img_id+".png\r\n");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		if(smiley.detectSmile(path))
 		{
 			if (smiley.getSmile_value()>50)
 			{
 				smile_counter = smile_counter +1;
+		
+				try {
+					writer.write("Smile detected\r\n");
+					writer.write("Smile Confidence:"+ smiley.getSmile_value()+"\r\n");
+					writer.write(smile_counter+" smile(s) detected\r\n");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				infoDisplay.append("Smile\n");
+				infoDisplay.append("Smile Confidence:"+ smiley.getSmile_value()+"\n");
+				infoDisplay.append(smile_counter+" smile(s) detected\n");
+				
+			
+				
+				
 				System.out.println("Smile");
 				System.out.println(smile_counter+" smile(s) detected");
 				smile_counter = 0;
+				
 			}
 			else
 			{
+				try {
+					writer.write("No smile \r\n");
+					
+				} catch (IOException e) {
+				
+					}
+				infoDisplay.append("No smile\n");
 				System.out.println("No smile");
 				smile_counter = 0;
 			}
 		}
 		else
 		{
+			try {
+				writer.write("Error during smile detection\r\n");
+				
+			} catch (IOException e) {
+			
+				}
+			infoDisplay.append("Error during smile detection, please refer to console\n");
 			System.out.println("Error during smile detection");
 		}
 		img_id=img_id+1;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally
+		{
+			try {
+				writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 		
 		
@@ -150,6 +240,8 @@ public class KinectVideoManager extends J4KSDK {
 		windowWidth=i;
 		windowHeight=j;
 	}
+	
+	
 	
 	
 
--- src/MyTest/MainWindow.java
+++ src/MyTest/MainWindow.java
@@ -1,28 +1,134 @@
 package MyTest;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.EventQueue;
+import java.io.IOException;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.SwingUtilities;
+
+import edu.ufl.digitalworlds.j4k.J4KSDK;
 
 
-public class MainWindow extends JFrame {
+
+public class MainWindow extends JDialog {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	KinectVideoManager kinect;
+	
 	public MainWindow() {
 
+
+        initKinect();
         initUI();
     }
 
-    private void initUI() {
-        
+    private void initKinect() {
+
+    	try {
+			kinect = new KinectVideoManager();
+		
+			
+			kinect.setServerUrl("https://rctest.ngrok.io");
+			
+			
+			}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	private void initUI() {
+    	
+    	JPanel basic = new JPanel();
+    	basic.setLayout(new BoxLayout(basic, BoxLayout.Y_AXIS));
+    	add(basic);
+    	
+    	JPanel infoPanel = new JPanel(new BorderLayout(5, 5));
+    	infoPanel.setBorder(BorderFactory.createEmptyBorder(15, 25, 15, 25));
+    	infoPanel.setMinimumSize(new Dimension(450,300));
+    	
+    	
+    	JTextArea pane = new JTextArea();
+    	String text = "Click on \"Start Smile Detection\" to start\n Results will be stored in the binary folder.";
+    	pane.setText(text);
+        pane.setEditable(false);
+       
+        kinect.setInfoDisplay(pane);
+        JScrollPane scroll = new JScrollPane(pane);
+    	infoPanel.add(scroll);
+    	
+    	basic.add(infoPanel);
+    	
+    	
+    	 JPanel bottom = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+         JButton start = new JButton("Start Smile Detection");
+         start.setMnemonic(KeyEvent.VK_S);
+         JButton stop = new JButton("Stop Smile Detection");
+         stop.setMnemonic(KeyEvent.VK_O);
+         JButton close = new JButton("Close");
+         close.setMnemonic(KeyEvent.VK_C);
+
+         bottom.add(start);
+         bottom.add(stop);
+         bottom.add(close);
+         basic.add(bottom);
+    	
+         close.addActionListener(new ActionListener() {
+        	    @Override
+        	    public void actionPerformed(ActionEvent event) {
+        	    	kinect.stop();
+        	    	System.exit(0);
+        	    }
+        	});
+         
+         start.addActionListener(new ActionListener() {
+        	    @Override
+        	    public void actionPerformed(ActionEvent event) {
+        	      
+        	       pane.setText("");
+        	       kinect.start(J4KSDK.COLOR);
+        	    }
+        	});
+         
+         stop.addActionListener(new ActionListener() {
+     	    @Override
+     	    public void actionPerformed(ActionEvent event) {
+     	       kinect.stop();
+     	     
+     	    }
+     	});
+    	
+    
         setTitle("Smile Detection Window");
-        setSize(300, 200);
+        setSize(450, 320);
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
     }
+    
+   
 
     public static void main(String[] args) {
 
